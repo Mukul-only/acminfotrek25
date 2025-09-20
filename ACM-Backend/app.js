55b3fa360b32595c13586bbf0f8cd8d3669c3e15
Vanshu Galhotra <vanshugalhotra3332@gmail.com>
2025-09-20 15:50:30 +0530

    backend - better logs

--- ACM-Backend/app.js
+++ ACM-Backend/app.js
@@ -106,5 +106,5 @@ app.use((error, req, res, next) => {
 // Start the server (unchanged)
 app.listen(PORT, "0.0.0.0", () => {
   logger.info("Server is starting...");
-  console.log(`Server is running on port ${PORT}`);
+  logger.info(`Server is running on port ${PORT}`);
 });
